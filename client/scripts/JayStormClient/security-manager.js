26d315ac72b50e82cac8a0faba8b8711e4d06cf1
PeterAronZentai <moswes22>
2012-09-07 14:29:21 +0200

    set permission fixes

--- client/scripts/JayStormClient/security-manager.js
+++ client/scripts/JayStormClient/security-manager.js
@@ -1,6 +1,5 @@
 $data.JayStormUI.AdminModel.extend("$data.JayStormClient.SecurityManager", {
-    constructor: function()
-    {
+    constructor: function () {
         var self = this;
 
         self.groups = ko.observableArray([]);
@@ -23,99 +22,96 @@ $data.JayStormUI.AdminModel.extend("$data.JayStormClient.SecurityManager", {
         }
 
 
-    self.tablePermissions = [
-        {
-            name: 'Read',
-            value: ko.observable()
-        },
-        {
-            name: 'Delete',
-            value: ko.observable()
-        },
-        {
-            name: 'Update',
-            value: ko.observable()
-        },
-        {
-            name: 'Create',
-            value: ko.observable()
-        },
-        {
-            name: 'Delete in a batch',
-            value: ko.observable()
-        },
-        {
-            name: 'Manage',
-            value: ko.observable()
-        }
+        self.tablePermissions = [
+            {
+                name: 'Read',
+                value: ko.observable()
+            },
+            {
+                name: 'Delete',
+                value: ko.observable()
+            },
+            {
+                name: 'Update',
+                value: ko.observable()
+            },
+            {
+                name: 'Create',
+                value: ko.observable()
+            },
+            {
+                name: 'Delete in a batch',
+                value: ko.observable()
+            },
+            {
+                name: 'Manage',
+                value: ko.observable()
+            }
 
-    ];
+        ];
 
-    self.selectedDatabase = ko.observable();
+        self.selectedDatabase = ko.observable();
 
-    self.tables = ko.observableArray([]);
+        self.tables = ko.observableArray([]);
 
-    self.selectedDatabase.subscribe(function(value) {
-        console.log(value);
-        var id = value.DatabaseID();
-        self.contextFactory().EntitySets
-            .filter(function(it) { return it.DatabaseID == this.id }, {id: id })
-            .toArray(self.tables);
-    });
+        self.selectedDatabase.subscribe(function (value) {
+            //console.log(value);
+            //var id = value.DatabaseID();
+            //self.contextFactory().EntitySets
+            //    .filter(function (it) { return it.DatabaseID == this.id }, { id: id })
+            //    .toArray(self.tables);
+        });
 
-    self.selectedTable = ko.observable();
-    self.selectedGroup = ko.observable();
+        self.selectedTable = ko.observable();
+        self.selectedGroup = ko.observable();
 
-    self.permissionRefresh = ko.observable();
+        self.permissionRefresh = ko.observable();
 
-    self.addPermission = function() {
-        //console.dir(p);
-        var c = self.contextFactory();
+        self.addPermission = function () {
+            //console.dir(p);
+            var c = self.createContext();
 
-        var dbIDs = self.selectedDatabase() ? [self.selectedDatabase().DatabaseID() ] :
-                                              self.databases().map( function(db) { return db.DatabaseID() });
-        var setIDs = self.selectedTable() ?
-                            Q.fcall(function() { return [{setID: self.selectedTable().EntitySetID(), dbID: dbIDs[0] }] }):
-                            c.EntitySets.filter("it.DatabaseID in this.ids",{ids: dbIDs}).map("{ setID: it.EntitySetID, dbID: it.DatabaseID}").toArray();
+            var dbIDs = self.selectedDatabase() ? [self.selectedDatabase().DatabaseID()] :
+                                                  self.databases().map(function (db) { return db.DatabaseID() });
 
-        var groupIDs = self.selectedGroup() ? [self.selectedGroup().GroupID()] :
-            self.groups().map( function(group) { return group.GroupID() });
+            //var setIDs = self.selectedTable() ?
+            //                    Q.fcall(function() { return [{setID: self.selectedTable().EntitySetID(), dbID: dbIDs[0] }] }):
+            //                    c.EntitySets.filter("it.DatabaseID in this.ids",{ids: dbIDs}).map("{ setID: it.EntitySetID, dbID: it.DatabaseID}").toArray();
 
-        self.items = ko.observableArray([]);
+            var groupIDs = self.selectedGroup() ? [self.selectedGroup().GroupID()] :
+                self.groups().map(function (group) { return group.GroupID() });
+
+            self.items = ko.observableArray([]);
 
 
-        var permissions = [];
-        Q.when(setIDs).then(function(setIDs) {
-            console.log(dbIDs, setIDs, groupIDs);
-            for(var j = 0; j < dbIDs.length; j++) {
-                for(var k = 0; k < setIDs.length; k++) {
-                    if (setIDs[k].dbID == dbIDs[j]) {
-                        for(var l = 0; l < groupIDs.length; l++) {
-                            var p = new c.Permissions.createNew({
-                                DatabaseID: dbIDs[j],
-                                EntitySetID: setIDs[k].setID,
-                                GroupID: groupIDs[l]
-                            });
-                            for(var z = 0; z < self.tablePermissions.length;z++) {
-                                p[self.tablePermissions[z].name] = self.tablePermissions[z].value();
-                            }
-                            c.add(p);
-                            var koItem = p.asKoObservable();
-                            self.items.push(koItem);
-                            console.log("pushdone");
-                        }
+            var permissions = [];
+            //        Q.when(setIDs).then(function(setIDs) {
+            //console.log(dbIDs, setIDs, groupIDs);
+            for (var j = 0; j < dbIDs.length; j++) {
+                //                for(var k = 0; k < setIDs.length; k++) {
+                //if (setIDs[k].dbID == dbIDs[j]) {
+                for (var l = 0; l < groupIDs.length; l++) {
+                    var p = new c.Permissions.createNew({
+                        DatabaseID: dbIDs[j],
+                        EntitySetID: 'NTA0NjMzOTdkNzJmMWMxOTdhMDAwMDA0',//setIDs[k].setID,
+                        GroupID: groupIDs[l]
+                    });
+                    for (var z = 0; z < self.tablePermissions.length; z++) {
+                        p[self.tablePermissions[z].name] = self.tablePermissions[z].value();
                     }
+                    c.add(p);
+                    var koItem = p.asKoObservable();
+                    self.items.push(koItem);
+                    console.log("pushdone");
                 }
             }
-            c.saveChanges( function() {
+            //    }
+            //}
+            c.saveChanges(function () {
                 self.rf(Math.random());
                 console.log("saved!");
             })
-        })
-
-
+        }
     }
-
-
 }
-});
+);
