9e0f989f4eb17d55c41d221b1b31f77c117e74b0
Gabor Dolla <agdolla@gmail.com>
2012-09-13 13:25:35 +0000

    latest && greatest

--- server/configuration.js
+++ server/configuration.js
@@ -1,12 +1,12 @@
-﻿var config = {
+var config = {
     admin: {
-        url:'dev-admin.jaystack.net',
+        url:'localhost',
         port: 3000,
         ssl: false,
         launch: '/launch'
     },
     dashboard: {
-        url: 'dev-dashboard.jaystack.com',
+        url: 'dashboard.jaystack.com',
         port :443,
         ssl: true,
         auth: '/auth.axd'
